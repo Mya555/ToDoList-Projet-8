3ebf0f95e0f6f2aa5fd6406242147eb92802e84e
Bella <text_me2@protonmail.com>
2019-04-03 21:03:06 +0200

    tests for default controller added

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,12 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testShowIndex()
     {
         $client = static::createClient();
         $crawler = $client->request( 'GET', '/' );
+
         $this->assertEquals( 200, $client->getResponse()->getStatusCode() );
-        $this->assertContains( 'Bienvenue sur Todo List', $crawler->filter( '.container h1' )->text() );
+        $this->assertContains( 'Bienvenue sur Todo List', $crawler->filter( '.container1 h1' )->text() );
+    }
+
+    public function testRedirectToTaskList()
+    {
+
+        $client = static::createClient();
+        $crawler = $client->request( 'GET', '/' );
+
+        $link = $crawler->selectLink( 'Consulter la liste des tâches à faire' )->link();
+        $crawler = $client->click( $link );
+
+        $info = $crawler->filter( 'h1' )->text();
+        $info = $string = trim( preg_replace( '/\s\s+/', ' ', $info ) ); // On retire les retours à la ligne pour faciliter la vérification
+
+        $this->assertSame( "Liste des tâches", $info );
+
     }
 }
 
