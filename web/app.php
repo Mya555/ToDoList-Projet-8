2c744a2ea4b3956daf7faca83221965cbc9b99c0
Bella <text_me2@protonmail.com>
2019-04-30 14:32:40 +0200

    depreciations removed

--- web/app.php
+++ web/app.php
@@ -6,8 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
@@ -15,3 +13,6 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+$kernel = new AppKernel('prod', false);
+Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8']);
+$request = Request::createFromGlobals();
